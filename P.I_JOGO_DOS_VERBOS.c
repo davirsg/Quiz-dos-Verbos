6e83140b0fea724f6daf9681cfef90a50bd81225
Davi Guimarães <65747521+davirsg@users.noreply.github.com>
2023-06-08 02:40:18 -0300

    Add files via upload
    Versão completa do jogo dos verbos, criado para um projeto de faculdade

---
+++ P.I_JOGO_DOS_VERBOS.c
@@ -0,0 +1,300 @@
+#include <stdio.h>
+#include <locale.h>
+
+struct jogo {
+	char resp1,resp2,resp3,resp4,resp5, resp6, resp7, resp8, resp9, resp10;
+
+int i, replayjogo;
+float acerto;
+};
+
+int main(){
+struct jogo jg;
+setlocale(LC_ALL,"");
+
+do{
+	//INTRODUÇÃO
+printf("**************************************************  QUIZ DOS VERBOS  **************************************************\n");
+printf("                         BEM-VINDO(A) AO QUIZ DOS VERBOS, UM JOGO SOBRE A LÍNGUA PORTUGUESA!\n\n");
+system("PAUSE");
+system("cls");
+
+//OBJETIVO DO JOGO
+printf("-Bem-vindo ao Quiz de Verbos!\n");
+printf("-Nosso objetivo é tornar o aprendizado da língua portuguesa divertido e envolvente, desafiando você a aprimorar suas habilidades verbais.\n");
+printf("-Teste seus conhecimentos, expanda seu vocabulário e conquiste novos níveis de domínio linguístico enquanto se diverte.\n");
+printf("-Embarque nessa jornada educativa e mostre seu talento com os verbos!\n\n");
+system("PAUSE");
+system("cls");
+
+
+//EXPLICANDO REGRAS
+printf("Regras do jogo:\n");
+printf("1. Objetivo: Vença obtendo o máximo de acertos.\n");
+printf("2. Formato: 10 perguntas de múltipla escolha com apenas uma resposta certa.\n");
+printf("3. Respostas: Escolha a opção correta.\n");
+printf("4. Pontuação: Cada acerto vale um ponto.\n");
+printf("5. Feedback: Você receberá o feedback da questão após cada resposta.\n");
+printf("6. Próxima pergunta: Avance automaticamente.\n");
+printf("7. Fim do jogo: Será exibida a pontuação final.\n");
+printf("8. Replay: Jogue novamente.\n\n");
+
+system("pause");
+system("cls");
+
+
+//QUESTÕES
+
+
+printf("QUESTÃO 01:\n");
+printf("Qual é a forma correta do verbo no presente do indicativo para a terceira pessoa do singular do verbo \"correr\" na norma culta da língua portuguesa?\n");
+printf("A) Corro\nB) Corres\nC) Corre\nD) Correm\n\nRESPOSTA: ");
+scanf(" %c", &jg.resp1);
+
+if(jg.resp1 ==  'C'|| jg.resp1 == 'c'){
+    printf("Correto, parabéns! :D\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"correr\" no presente do indicativo para a terceira pessoa do singular é \"Corre\".\n");
+    jg.acerto++;
+    
+} else {
+    printf("INCORRETO!\nMAS NÃO DESANIME, VOCÊ É CAPAZ!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"correr\" no presente do indicativo para a terceira pessoa do singular é \"Corre\".\n");
+}
+
+system("PAUSE");
+system("cls");
+
+
+
+printf("QUESTÃO 02:\nQual alternativa completa corretamente a frase com o verbo no tempo e modo adequados?\n\"Ontem eu ____ ao parque com meus amigos.\"\nA) fui\nB) iria\nC) foi\nD) vai\n\nRESPOSTA: ");
+scanf(" %c", &jg.resp2);
+
+if(jg.resp2 == 'a' || jg.resp2 == 'A'){
+    printf("Parabéns, você acertou! XD\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("Para completar corretamente a frase, devemos usar o verbo \"fui\" no pretérito perfeito do indicativo.\n");
+    jg.acerto++;
+} 
+else {
+    printf("INCORRETO!\nMas não se preocupe, erros fazem parte do aprendizado, você vai conseguir! \n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("Para completar corretamente a frase, devemos usar o verbo \"fui\" no pretérito perfeito do indicativo.\n");
+}
+
+system("PAUSE");
+system("cls");
+
+
+printf("QUESTÃO 03:\nQual é o tempo verbal da frase \"Eles estavam assistindo ao filme quando você chegou\"?\nA) Pretérito perfeito\nB) Futuro do presente\nC) Pretérito imperfeito\nD) Presente do indicativo\nRESPOSTA: ");
+scanf(" %c", &jg.resp3);
+
+if(jg.resp3 == 'c' || jg.resp3 == 'C'){
+    printf("Incrível! Continue assim, sempre dando o seu melhor.\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O tempo verbal da frase é o pretérito imperfeito, indicando uma ação que estava ocorrendo no passado.\n");
+    jg.acerto++;
+} 
+else {
+    printf("INCORRETO!\nMas tudo bem, cada erro é um passo em direção ao sucesso. Continue persistindo! \n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O tempo verbal da frase é o pretérito imperfeito, indicando uma ação que estava ocorrendo no passado.\n");
+}
+
+system("PAUSE");
+system("cls");
+
+
+printf("QUESTÃO 04: Qual é o verbo que está na forma infinitiva na frase \"Nós queremos viajar nas férias\"?\n");
+printf("a) queremos\n");
+printf("b) viajar\n");
+printf("c) nas\n");
+printf("d) férias\n");
+printf("Resposta: ");
+scanf(" %c", &jg.resp4);
+
+if(jg.resp4 == 'b' || jg.resp4 == 'B'){
+    printf("Parabéns! Seu esforço e dedicação estão dando resultado.\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O verbo \"viajar\" está na forma infinitiva na frase \"Nós queremos viajar nas férias\".\n");
+    jg.acerto++;
+} 
+else {
+    printf("INCORRETO!\nMas lembre-se de que o erro é uma oportunidade para aprender algo novo. Continue tentando! \n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O verbo \"viajar\" está na forma infinitiva na frase \"Nós queremos viajar nas férias\".\n");
+}
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 05: Qual é a forma correta do verbo no imperativo afirmativo para a segunda pessoa do singular do verbo \"fazer\"?\n");
+printf("a) Faça\n");
+printf("b) Faze\n");
+printf("c) Fazer\n");
+printf("d) Faz\n");
+printf("Resposta: ");
+scanf(" %c", &jg.resp5);
+
+if (jg.resp5 == 'a' || jg.resp5 == 'A') {
+    printf("Parabéns! Você está arrasando nas respostas!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"fazer\" no imperativo afirmativo para a segunda pessoa do singular é \"Faça\".\n");
+    jg.acerto++;
+} else {
+    printf("Incorreto!\nMas não se preocupe, continue se esforçando e você vai melhorar!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"fazer\" no imperativo afirmativo para a segunda pessoa do singular é \"Faça\".\n");
+}
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 06: Qual é o tempo verbal da frase \"Eu estudei para a prova\"?\n");
+printf("a) Presente do indicativo\n");
+printf("b) Futuro do presente\n");
+printf("c) Pretérito perfeito\n");
+printf("d) Pretérito mais-que-perfeito\n");
+printf("Resposta: ");
+scanf(" %c", &jg.resp6);
+
+if (jg.resp6 == 'c' || jg.resp6 == 'C') {
+    printf("Parabéns! Você está mandando muito bem!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O tempo verbal da frase é o pretérito perfeito, indicando uma ação concluída no passado.\n");
+    jg.acerto++;
+} else {
+    printf("Incorreto!\nNão se preocupe, continue praticando e você vai melhorar cada vez mais!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O tempo verbal da frase é o pretérito perfeito, indicando uma ação concluída no passado.\n");
+}
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 07: Qual é o verbo que está na forma infinitiva na frase \"Ela precisa estudar para a prova\"?\n");
+printf("a) precisa\n");
+printf("b) estudar\n");
+printf("c) para\n");
+printf("d) prova\n");
+printf("Resposta: ");
+scanf(" %c", &jg.resp7);
+
+if (jg.resp7 == 'b' || jg.resp7 == 'B') {
+    printf("Parabéns! Você está arrasando nas respostas!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O verbo \"estudar\" está na forma infinitiva na frase \"Ela precisa estudar para a prova\".\n");
+    jg.acerto++;
+} else {
+    printf("Incorreto!\nMas não desanime, continue estudando e você vai melhorar!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("O verbo \"estudar\" está na forma infinitiva na frase \"Ela precisa estudar para a prova\".\n");
+}
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 08: Qual é a forma correta do verbo no presente do indicativo para a terceira pessoa do plural do verbo \"beber\"?\n");
+printf("a) bebes\n");
+printf("b) bebem\n");
+printf("c) bebe\n");
+printf("d) bebem\n");
+printf("Resposta: ");
+scanf(" %c", &jg.resp8);
+
+if (jg.resp8 == 'd' || jg.resp8 == 'D') {
+    printf("Parabéns! Você está arrasando nas respostas!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"beber\" no presente do indicativo para a terceira pessoa do plural é \"bebem\".\n");
+    jg.acerto++;
+} else {
+    printf("Incorreto!\nNão se preocupe, continue se esforçando e você vai melhorar!\n\n");
+    printf("EXPLICAÇÃO\n");
+    printf("A forma correta do verbo \"beber\" no presente do indicativo para a terceira pessoa do plural é \"bebem\".\n");
+}
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 09: Escolha a opção que apresenta um verbo no gerúndio:\n");
+    printf("a) correu\n");
+    printf("b) dançar\n");
+    printf("c) dançando\n");
+    printf("d) cantou\n");
+    printf("Resposta: ");
+    scanf(" %c", &jg.resp9);
+
+    if (jg.resp9 == 'c' || jg.resp9 == 'C') {
+        printf("Parabéns! Você acertou!\n\n");
+        printf("EXPLICAÇÃO\n");
+        printf("\"Dançando\" é um verbo no gerúndio, indicando a ação de dançar em progresso ou simultânea a outra ação.\n");
+    } else {
+        printf("Incorreto!\nNão se preocupe, continue se esforçando e você vai melhorar!\n\n");
+        printf("EXPLICAÇÃO\n");
+        printf("\"Dançando\" é um verbo no gerúndio, indicando a ação de dançar em progresso ou simultânea a outra ação.\n");
+    }
+
+system("PAUSE");
+system("cls");
+
+printf("QUESTÃO 10: Identifique o verbo na frase abaixo:\n");
+    printf("\"A menina corria alegremente no parque.\"\n");
+    printf("a) menina\n");
+    printf("b) corria\n");
+    printf("c) alegremente\n");
+    printf("d) parque\n\n");
+    printf("Resposta: ");
+    scanf(" %c", &jg.resp10);
+    
+    if (jg.resp10 == 'b' || jg.resp10 == 'B') {
+        printf("Correto! O verbo na frase é \"corria\".\n");
+    } else {
+        printf("Incorreto! O verbo na frase é \"corria\".\n");
+    }
+    
+system("PAUSE");
+system("cls");
+
+//RESULTADO FINAL
+printf("************************************************** RESULTADO *************************************************************\n\n");
+printf("Sua pontuação final é: %.2f\n\n", jg.acerto);
+printf("Você acertou %.0f de 10 questões.\n\n", jg.acerto);
+
+if (jg.acerto >= 6) {
+    printf("Parabéns! Você mandou muito bem no jogo dos verbos!\n\n");
+} else {
+    printf("Não desanime! Continue praticando e você vai melhorar cada vez mais!\n\n");
+}
+
+system("PAUSE");
+system("cls");
+
+
+//REPLAY
+printf("Deseja jogar novamente?\n");
+printf("1 - SIM\n");
+printf("2 - NÃO\n");
+printf("Escolha uma opção: ");
+scanf("%d", &jg.replayjogo);
+
+
+printf("\n");
+system("cls");
+
+
+} while (jg.replayjogo == 1);
+system("cls");
+printf("Obrigado por jogar! :)\n");
+printf("Jogo criado por: Érick Mello, Davi Guimarães, João Cunha, Wrick Nazário, João Paulo.");
+return 0;
+}
+
+
+
+
+
+
+
+
+
